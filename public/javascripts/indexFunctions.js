217cb2f8c39465b3c572e8c2a2260b1403358685
Pebanh <peter.banh95@gmail.com>
2020-06-25 02:32:20 -0400

    Add styling and content to the info-box, and styling to the create page and login.

--- public/javascripts/indexFunctions.js
+++ public/javascripts/indexFunctions.js
@@ -1,12 +1,13 @@
-// DOM ELEMENTS
+//POP-UP MENU
+//DOM Elements
 let modConEl = document.querySelector("#modules-container");
 let formEl = null;
 let delPopUp = document.getElementById("pop-up-delete");
 let delBackdrop = document.getElementById("delete-backdrop");
 let delYes = document.getElementById("del-yes");
 let delNo = document.getElementById("del-no");
-let infoBox = document.getElementById("info-box");
 
+// Events
 modConEl.onclick = function (event) {
   if (!event.target.classList.contains("deleteBtn")) return;
   formEl = event.target.parentNode;
@@ -25,6 +26,25 @@ delPopUp.onclick = function (event) {
   }
 };
 
-infoBox.onclick = function (event) {
-  console.log("hi");
-};
+//INFO BOX
+let infoBox = document.getElementById("info-box");
+let arrows = document.querySelectorAll(".arrow");
+let state = 1;
+
+arrows.forEach((arrow) => {
+  arrow.onclick = function (event) {
+    let circle = document.getElementById("circle-" + state);
+    let box = document.getElementById("box-" + state);
+    box.style.display = "none";
+    circle.style.background = "rgb(109, 109, 109)";
+    if (event.target.id == "next") {
+      if (++state == 4) state = 1;
+    } else if (event.target.id == "prev") {
+      if (--state == 0) state = 3;
+    }
+    box = document.getElementById("box-" + state);
+    circle = document.getElementById("circle-" + state);
+    box.style.display = "flex";
+    circle.style.background = "#ffde00ff";
+  };
+});
