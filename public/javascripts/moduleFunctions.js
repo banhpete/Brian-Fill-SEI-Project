4391f2f32bc7e961bae33b176d5bb5e06da2984d
Pebanh <peter.banh95@gmail.com>
2020-06-28 00:20:41 -0400

    Add style to module page and refactor JS functionality

--- public/javascripts/moduleFunctions.js
+++ public/javascripts/moduleFunctions.js
@@ -1,16 +1,13 @@
-// DOM ELEMENTS
+// DELETE POP UP
+// Dom Elements
 let modConEl = document.querySelector("#module-container");
 let formEl = null;
 let delPopUp = document.getElementById("pop-up-delete");
 let delBackdrop = document.getElementById("delete-backdrop");
 let delYes = document.getElementById("del-yes");
 let delNo = document.getElementById("del-no");
-let gnEl = document.getElementById("gn-container");
-let gnBtn = document.getElementById("gn-btn");
-let quizEl = document.getElementById("quiz-container");
-let quizBtn = document.getElementById("quiz-btn");
-let usefulBtn = document.getElementById("useful-btn");
 
+// Events
 modConEl.onclick = function (event) {
   if (!event.target.classList.contains("deleteBtn")) return;
   formEl = event.target.parentNode;
@@ -29,46 +26,72 @@ delPopUp.onclick = function (event) {
   }
 };
 
-quizBtn.onclick = function (event) {
-  console.log(event);
-  gnEl.style.display = "none";
-  quizEl.style.display = "block";
-};
+// // QUIZ-GUIDED NOTES TRANSITION
+// // Dom Elements
+// let gnEl = document.getElementById("gn-container");
+// let gnBtn = document.getElementById("gn-btn");
+// let quizEl = document.getElementById("quiz-container");
+// let quizBtn = document.getElementById("quiz-btn");
 
-gnBtn.onclick = function (event) {
-  gnEl.style.display = "block";
-  quizEl.style.display = "none";
-};
+// // Events
+// quizBtn.onclick = function (event) {
+//   console.log(event);
+//   gnEl.style.display = "none";
+//   quizEl.style.display = "block";
+// };
 
-gnEl.onchange = function (event) {
-  if (event.target.tagName != "INPUT") return;
-  if (event.target.value == event.target.id) {
-    event.target.style.background = "green";
-  }
-};
+// gnBtn.onclick = function (event) {
+//   gnEl.style.display = "block";
+//   quizEl.style.display = "none";
+// };
 
-if (usefulBtn) {
-  usefulBtn.onclick = function (event) {
-    let values = usefulBtn.value.split("-");
-    let uid = values[1];
-    let mid = values[0];
+// gnEl.onchange = function (event) {
+//   if (event.target.tagName != "INPUT") return;
+//   if (event.target.value == event.target.id) {
+//     event.target.style.background = "green";
+//   }
+// };
 
-    if (uid.length < 15) {
-      return;
-    }
-    fetch("http://localhost:3000/modules/useful/" + uid + "/" + mid)
-      .then(function (response) {
-        return response.json();
-      })
-      .then(function (response) {
-        if (response.index == -1) {
-          usefulBtn.innerText = response.total + " Found Useful";
-        } else {
-          usefulBtn.innerText = "Did you Find This Useful";
-        }
-      })
-      .catch(function (err) {
-        return;
-      });
+// // LIKE BUTTON
+// // Dom Elements
+// let usefulBtn = document.getElementById("useful-btn");
+
+// // Events
+// if (usefulBtn) {
+//   usefulBtn.onclick = function (event) {
+//     let values = usefulBtn.value.split("-");
+//     let uid = values[1];
+//     let mid = values[0];
+
+//     if (uid.length < 15) {
+//       return;
+//     }
+//     fetch("http://localhost:3000/modules/useful/" + uid + "/" + mid)
+//       .then(function (response) {
+//         return response.json();
+//       })
+//       .then(function (response) {
+//         if (response.index == -1) {
+//           usefulBtn.innerText = response.total + " Found Useful";
+//         } else {
+//           usefulBtn.innerText = "Did you Find This Useful";
+//         }
+//       })
+//       .catch(function (err) {
+//         return;
+//       });
+//   };
+// }
+
+// MORE INFO
+// Dom Elements
+let arrows = document.querySelectorAll(".arrow");
+
+arrows.forEach((arrow) => {
+  arrow.onclick = function (event) {
+    console.log("hi");
+    event.target.parentElement.previousElementSibling.classList.toggle(
+      "module-info-activate"
+    );
   };
-}
+});
